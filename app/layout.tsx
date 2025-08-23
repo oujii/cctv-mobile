7454da0f09a0cd2fdf2237a82ebe74f3b1da8bf3
Carl <carl.waerner@hyperisland.se>
2025-08-23 23:16:36 +0200

    pwa loosening

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,23 +45,6 @@ export default function RootLayout({
         className={`${inter.variable} font-inter antialiased`}
       >
         {children}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              if ('serviceWorker' in navigator) {
-                window.addEventListener('load', function() {
-                  navigator.serviceWorker.register('/sw.js')
-                    .then(function(registration) {
-                      console.log('SW registered: ', registration);
-                    })
-                    .catch(function(registrationError) {
-                      console.log('SW registration failed: ', registrationError);
-                    });
-                });
-              }
-            `,
-          }}
-        />
       </body>
     </html>
   );
